64685cacfac9f8adebcd71d0711445cc120c55dc
martine-stdo <2062461560@qq.com>
2023-06-12 19:47:10 +0800

    LoginController中登录函数添加了注解使其可以读取信息，新增UserDetailsServiceImpl类解决堆栈溢出问题

--- backend/src/main/java/com/iss/edu/online/controller/LoginController.java
+++ backend/src/main/java/com/iss/edu/online/controller/LoginController.java
@@ -3,6 +3,8 @@ package com.iss.edu.online.controller;
 import com.iss.edu.online.Service.LoginService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Map;
@@ -12,7 +14,7 @@ public class LoginController {
     @Autowired
     private LoginService loginService;
     @PostMapping("/user/token")
-    public Map<String, String> getToken(Map<String, String> map) {
+    public Map<String, String> getToken(@RequestBody Map<String, String> map) {
         String username = map.get("username");
         String password = map.get("password");
 
