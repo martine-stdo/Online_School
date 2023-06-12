f5b5ce532a76d559516e366d2c14baf39257f3b5
martine-stdo <2062461560@qq.com>
2023-06-12 18:42:01 +0800

    修改了loginService

--- backend/src/main/java/com/iss/edu/online/Service/LoginService.java
+++ backend/src/main/java/com/iss/edu/online/Service/LoginService.java
@@ -3,6 +3,7 @@ package com.iss.edu.online.Service;
 import com.iss.edu.online.mapper.UserDao;
 import com.iss.edu.online.model.User;
 import com.iss.edu.online.utils.JwtUtil;
+import com.iss.edu.online.utils.UserDetailsImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -21,7 +22,9 @@ public class LoginService {
     public Map<String, String> getToken(String username, String password) {
         UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(username, password);
         Authentication authenticate = authenticationManager.authenticate(authenticationToken);
-        User user = userDao.selectUser(username);
+
+        UserDetailsImpl userDetails = (UserDetailsImpl) authenticate.getPrincipal();
+        User user = userDetails.getUser();
         String jwt = JwtUtil.createJWT(user.getId().toString());
         Map<String, String> map = new HashMap<>();
         map.put("error_message","success");
