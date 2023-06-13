0567d83d3cb6b8509a3f3fee6173f1348d11a318
影子你陪着我累吗 <1477649017@qq.com>
2023-06-13 09:22:55 +0800

    梳理JWT逻辑

--- backend/src/main/java/com/iss/edu/online/EduOnlineProjectApplication.java
+++ backend/src/main/java/com/iss/edu/online/EduOnlineProjectApplication.java
@@ -3,9 +3,8 @@ package com.iss.edu.online;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(exclude = {org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class})
+@SpringBootApplication
 public class EduOnlineProjectApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(EduOnlineProjectApplication.class, args);
     }
