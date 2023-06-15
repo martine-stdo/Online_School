82bee3c0a98fac98b4b614976100aa711bdbe68a
martine-stdo <2062461560@qq.com>
2023-06-15 11:28:24 +0800

    完成课程信息查询接口

--- backend/src/main/java/com/iss/edu/online/controller/CourseController/ListOfCourseInfo.java
+++ backend/src/main/java/com/iss/edu/online/controller/CourseController/ListOfCourseInfo.java
@@ -1,21 +1,40 @@
 package com.iss.edu.online.controller.CourseController;
 
 import com.iss.edu.online.common.ResultResponse;
+import com.iss.edu.online.mapper.CourseDao;
 import com.iss.edu.online.model.Course;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.iss.edu.online.model.CourseVO;
+import com.iss.edu.online.model.Employee;
+import com.iss.edu.online.model.PageRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 //以列表的形式返回所有课程信息
 @RestController
 public class ListOfCourseInfo {
+    @Autowired
+    private CourseDao courseDao;
 
-    @PostMapping("/course/list")
-    public ResultResponse listAllCourseInfo() {
-
-
-        return null;
+    @PostMapping("/queryCourse")
+    public ResultResponse queryCourses(@RequestBody PageRequest pageRequest) {
+        Integer pageNum = pageRequest.getPageNum();
+        Integer pageSize = pageRequest.getPageSize();
+        if (pageNum == null || pageSize == null) {
+            return ResultResponse.fail(-1, "参数错误!");
+        }
+        int start = (pageNum - 1) * pageSize;
+        List<CourseVO> courses = courseDao.queryCourse(start, pageSize);
+        int total = courseDao.count();
+        int totalPage = (int) Math.ceil(total * 1.0 / pageSize);
+        Map<String, Object> data = new HashMap<>();
+        data.put("totalPage", totalPage);
+        data.put("courses", courses);
+        return ResultResponse.success(data);
     }
-
 }
+
+
--- backend/src/main/java/com/iss/edu/online/mapper/TeacherDao.java
+++ backend/src/main/java/com/iss/edu/online/mapper/TeacherDao.java
@@ -1,4 +1,10 @@
 package com.iss.edu.online.mapper;
 
+import com.iss.edu.online.model.Course;
+import com.iss.edu.online.model.Employee;
+
+import java.util.List;
+
 public interface TeacherDao {
+
 }
