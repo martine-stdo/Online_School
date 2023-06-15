a3bd9c06ebc2198dea39d6a044055f2b37a6b355
martine-stdo <2062461560@qq.com>
2023-06-15 10:11:32 +0800

    创建teacherdao

--- backend/src/main/java/com/iss/edu/online/controller/CourseController/ListOfCourseInfo.java
+++ backend/src/main/java/com/iss/edu/online/controller/CourseController/ListOfCourseInfo.java
@@ -1,5 +1,21 @@
 package com.iss.edu.online.controller.CourseController;
-//以列表的形式返回所有学生信息
+
+import com.iss.edu.online.common.ResultResponse;
+import com.iss.edu.online.model.Course;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+//以列表的形式返回所有课程信息
+@RestController
 public class ListOfCourseInfo {
+
+    @PostMapping("/course/list")
+    public ResultResponse listAllCourseInfo() {
+
+
+        return null;
+    }
 
 }
