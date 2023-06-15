a1daae6aef8a7d75d60458a82f87b80bd4d9f488
Errore <error_wings@qq.com>
2023-06-15 16:29:17 +0800

    Index

--- web/src/views/Index.vue
+++ web/src/views/Index.vue
@@ -1,14 +1,45 @@
 <template>
-  <el-container style="width: 100%;height: 100%;">
-    <el-header style="height: 70px;background-color: black;
-    display: flex;justify-content: space-between;align-items: center;">
-      <img src="../assets/images/logo.png" style="object-fit: contain; width: 40px;height: 40px">
-      <div style="width: 8%;height: 100%;display: flex;justify-content: space-around;align-items: center;">
-        <img src="../assets/images/zz.jpg" style="object-fit: contain;width: 40px;height: 40px;border-radius:50%;"/>
+  <el-container style="width: 100%; height: 100%">
+    <el-header
+      style="
+        height: 70px;
+        background-color: black;
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+      "
+    >
+      <div
+        style="
+          display: flex;
+          justify-content: space-between;
+          align-items: center;
+        "
+      >
+        <img
+          src="../assets/images/logo.png"
+          style="object-fit: contain; width: 40px; height: 40px"
+        />
+        <span style="color: white; padding-left: 15px"
+          ><h1>XX管理系统</h1></span
+        >
+      </div>
+      <div
+        style="
+          height: 100%;
+          display: flex;
+          justify-content: space-around;
+          align-items: center;
+        "
+      >
+        <el-avatar :style="`background:${extractColorByName('管理员')}`">
+          {{ user.username }}
+        </el-avatar>
+        <div  style="width: 15px"></div>
         <el-dropdown>
-            <span class="el-dropdown-link" style="color: white;">
-              {{ user.nickname }}<i class="el-icon-arrow-down el-icon--right"></i>
-            </span>
+          <span class="el-dropdown-link" style="color: white">
+            你好，{{ user.username }}<i class="el-icon-arrow-down el-icon--right"></i>
+          </span>
           <el-dropdown-menu>
             <el-dropdown-item>退出登录</el-dropdown-item>
           </el-dropdown-menu>
@@ -16,26 +47,32 @@
       </div>
     </el-header>
     <el-container>
-      <el-aside width="15vw" style="display: flex;flex-direction: column;align-items: stretch;border-right: 1px solid #d9d9d9;">
-        <el-menu router :default-active="$route.path" style="border:0;">
+      <el-aside
+        width="15vw"
+        style="
+          display: flex;
+          flex-direction: column;
+          align-items: stretch;
+          border-right: 1px solid #d9d9d9;
+        "
+      >
+        <el-menu router :default-active="$route.path" style="border: 0">
           <el-menu-item index="1" route="/main">
-            <i class="el-icon-goods"/>
+            <i class="el-icon-goods" />
             <span slot="title">主页</span>
           </el-menu-item>
           <el-menu-item index="2" route="/courses">
-            <i class="el-icon-data-analysis"/>
+            <i class="el-icon-data-analysis" />
             <span slot="title">我的课程</span>
           </el-menu-item>
           <el-menu-item index="3" route="/admin">
-            <i class="el-icon-data-analysis"/>
+            <i class="el-icon-data-analysis" />
             <span slot="title">我的班级</span>
           </el-menu-item>
         </el-menu>
       </el-aside>
       <el-main>
-
-        <router-view/>
-
+        <router-view />
       </el-main>
     </el-container>
   </el-container>
@@ -51,13 +88,23 @@ export default {
     return {
       openRouter: true,
       isAdmin: false,
-      user: {},
-    }
+      user: {
+        username:"管理员"
+      },
+    };
   },
-  methods: {}
-}
+  methods: {
+    extractColorByName(name) {
+      var temp = [];
+      temp.push("#");
+      for (let index = 0; index < name.length; index++) {
+        temp.push(parseInt(name[index].charCodeAt(0), 10).toString(16));
+      }
+      return temp.slice(0, 5).join("").slice(0, 4);
+    },
+  },
+};
 </script>
 
 <style scoped>
-
 </style>
