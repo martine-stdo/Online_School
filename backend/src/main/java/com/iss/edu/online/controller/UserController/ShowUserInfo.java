47afc66a8b3438d358cbb75f530accf8eea1054e
martine-stdo <2062461560@qq.com>
2023-06-16 09:39:52 +0800

    将Authorization修改为Access_token

--- backend/src/main/java/com/iss/edu/online/controller/UserController/ShowUserInfo.java
+++ backend/src/main/java/com/iss/edu/online/controller/UserController/ShowUserInfo.java
@@ -16,7 +16,7 @@ public class ShowUserInfo {
     @Autowired private CheckAdmin checkAdmin;
     @Autowired private UserDao userDao;
     @GetMapping ("/user/info")
-    public ResultResponse userInfo(@RequestHeader("Authorization") String authorization) throws Exception {
+    public ResultResponse userInfo(@RequestHeader("Access_token") String authorization) throws Exception {
         String jwt = checkAdmin.extractJwt(authorization);
         Claims claims = JwtUtil.parseJWT(jwt);
         String userId = claims.getSubject();
