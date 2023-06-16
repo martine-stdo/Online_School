e098d450e06fbeef3b2161845b1d154bd9c81ba9
Errore <error_wings@qq.com>
2023-06-16 09:46:20 +0800

    管理界面

--- web/src/views/Admin.vue
+++ web/src/views/Admin.vue
@@ -1,35 +1,27 @@
 <template>
   <div style="width: 100%;height: 100%;display: flex;flex-direction: column;">
 
-    <span style="font-size: 26px; height: 40px; font-weight: 500;">学员管理</span>
+    <span style="font-size: 26px; height: 40px; font-weight: 500;">课程管理</span>
 
     <el-row type="flex" align="center" justify="space-between" style="width: 100%;margin-top: 20px;">
       <el-form :inline="true">
-        <el-form-item label="关键词" style="font-weight: bold;">
-          <el-input v-model="keyword" placeholder="输入关键字进行模糊匹配" style="width: 250px;"></el-input>
-        </el-form-item>
-        <el-form-item label="模糊匹配" style="font-weight: bold;margin-left: 20px;">
-          <el-switch :width="50" :value="true" active-color="#13ce66" inactive-color="#D5D5D5FF"/>
-        </el-form-item>
         <el-form-item style="margin-left: 20px;">
           <el-button type="primary">查询</el-button>
         </el-form-item>
       </el-form>
       <div>
-        <el-button type="primary">创建用户</el-button>
-        <el-button type="danger" @click="deletes">删除用户</el-button>
+        <el-button type="primary">创建课程</el-button>
+        <el-button type="danger" @click="deletes">删除课程</el-button>
       </div>
     </el-row>
 
     <el-table ref="studentTable" :data="tableData" border style="width: 100%">
       <el-table-column type="selection" width="55"/>
-      <el-table-column prop="empno" label="员工编号" width="155"/>
-      <el-table-column prop="ename" label="员工姓名" width="250"/>
-      <el-table-column prop="job" label="员工职位" width="250"/>
-      <el-table-column prop="mgr" label="上级职位" width="250"/>
-      <el-table-column prop="sal" label="员工薪资" width="250"/>
-      <el-table-column prop="comm" label="岗位奖金" width="250"/>
-      <el-table-column prop="department.dname" label="所属部门" width="250"/>
+      <el-table-column prop="courseId" label="课程编号" width="155"/>
+      <el-table-column prop="courseName" label="课程名名" width="250"/>
+      <el-table-column prop="courseDescription" label="描述" width="250"/>
+      <el-table-column prop="courseStartTime" label="开始时间" width="250"/>
+      <el-table-column prop="courseEndTime" label="结束时间" width="250"/>
     </el-table>
     <div style="width: 100%; display: flex;justify-content: center;">
 
@@ -43,9 +35,9 @@
 
 <script>
 export default {
-  name: "Users",
+  name: "Admin",
   created() {
-    this.queryEmployees(this.currentPage, this.pageSize);
+    this.queryCourses(this.currentPage, this.pageSize);
   },
   data() {
     return {
@@ -65,11 +57,12 @@ export default {
       console.log("pageNum:", pageNum)
       this.queryEmployees(pageNum, this.pageSize)
     },
-    queryEmployees(currentPage, pageSize) {
-      this.$axios.get('/api/queryEmployees/' + currentPage + '/' + pageSize).then(result => {
-        this.tableData = result.data.employees;
-        this.total = result.data.total;
-      });
+    queryCourses(currentPage, pageSize) {
+      this.$axios
+      .post("/queryCourse", { pageNum: currentPage, pageSize: pageSize })
+      .then((res)=>{
+        this.tableData = res.data.courses;
+      })
     },
     deletes() {
       let rows = this.$refs['studentTable'].selection;
